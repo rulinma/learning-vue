ff5ca1997d201b6157a3d4794e7d7c827d523148
rulinma <rulinma@gmail.com>
2023-03-07 21:48:53 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -3,9 +3,15 @@ import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store";
+import Vant from "vant";
+import "vant/lib/index.css";
 
 const app = createApp(App);
 
 app.use(ElementPlus);
 app.use(router);
+app.use(store);
+
+app.use(Vant);
 app.mount("#app");
--- src/pages/Login/Error.vue
+++ src/pages/Login/Error.vue
@@ -1,17 +1,42 @@
 <template>
     <div>
-        <h1>Error {{ msg }}</h1>
+        error
+        <van-image round lazy-load width="200" height="200" :src="img">
+            <template #loading>
+                <van-loading type="spinner" size="30" />
+            </template>
+        </van-image>
+
+        <br>
+
+        <van-button type="primary" icon="search" zise="mini" text="随机dog" :loading="loading" @click="getImg"
+            color="linear-gradient(to right, #ff6034, #ee0a24)" loading-text="loading..." />
     </div>
 </template>
+
 <script>
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+
 export default {
-    data() {
+    setup() {
+        console.log("vue user store");
+        const { state, getters, commit, dispatch } = useStore()
+        let img = computed(() => getters['User/getRandomImg'])
+        let loading = ref(false)
+
+        const getImg = async () => {
+            console.log("get image");
+            loading.value = true
+            await dispatch('User/setRandomImg')
+            loading.value = false
+        }
+
         return {
-            msg: null
-        };
+            img,
+            loading,
+            getImg,
+        }
     },
-    created() {
-        this.msg = this.$route.query.message;
-    }
-};
+}
 </script>
